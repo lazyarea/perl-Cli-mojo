f8fb887e8bee520772e2c5b807e1e58cec215691
My Name <kazuhisa@lazyarea.com>
2016-12-19 11:58:27 +0900

    add update kihon-member

--- lib/ImportCsv/Data/Dtb/Customer.pm
+++ lib/ImportCsv/Data/Dtb/Customer.pm
@@ -94,16 +94,17 @@ sub create_or_updateCustomer
     if ( !$data ){
         if ($file =~ /^NVH_KIHON/i) {
             $ret = &createMember($pg,$line);
-#        }elsif($file =~ /^FCH_KIHON/i){
+        }elsif($file =~ /^FCH_KIHON/i){
+            $ret = &createOnline($pg,$line);
         }
 
 #        return $ret;
     }else{
         if ($file =~ /^NVH_KIHON/i) {
             $ret = &updateMember($pg,$line);
-#        }elsif($file =~ /^FCH_KIHON/i){
+        }elsif($file =~ /^FCH_KIHON/i){
+            $ret = &updateOnline($pg,$line);
         }
-        #&updateCustomer($pg, $line);
         return undef;
     }
 }
@@ -177,6 +178,46 @@ sub createMember
     return $nextv;
 }
 
+sub createOnline
+{
+    my ($pg,$line) =@_;
+    my $utils = ImportCsv::Commons::Utils->new;
+    my $dt = Moment->now->get_dt();
+    my $sex = $line->[1];
+    for(my $i=0; $i< keys $line; $i++) {$line->[$i] =~ s/'/''/g;}
+    # $line->[19]保有ポイントTBD
+    $line->[19] = 0 if ( !$line->[19]);
+    # $line->[20]ポイント有効期限
+    my $pexpired = $line->[20];
+    if ( length($line->[20]) ){
+        my $y = substr($line->[20],0,4);
+        my $m = substr($line->[20],4,2);
+        my $d = substr($line->[20],6,2);
+        $pexpired = sprintf("%4s-%2s-%2s 00:00:00", $y,$m,$d);
+    }else{$pexpired = '1970-01-01 00:00:00';}
+    # $line->[21]支払い状況は会員の場合NULL
+    # $line->[22]会員状況(2)
+    $line->[22] = ($line->[22] =~ s/^0+//);
+    # secret_key
+    my $ramdom = $utils->generate_str();
+    my $sql = 'INSERT INTO dtb_customer(status,sex,pref,name01,name02,kana01,kana02,company_name,company_name2,zip01,zip02,addr01,addr02,addr03,tel01,tel02,fax01,note,create_date,update_date,del_flg,client_code,client_code,customer_type_id,customer_kind_id,customer_situation_id,customer_division_id,black_rank,markup_rate,realize_point,point_expiration_date,secret_key) VALUES(';
+    $sql .= "$line->[22], $line->[1], $line->[2], '$line->[3]', '$line->[4]', '$line->[5]', '$line->[6]', '$line->[7]', '$line->[8]', '$line->[9]', '$line->[10]', '$line->[11]', '$line->[12]', '$line->[13]', '$line->[14]', '$line->[15]', '$line->[16]', '$line->[24]', '$dt', '$dt', 0, null, '$line->[17]', $line->[0], 1, 1, 1, null, 1, $line->[19], '$pexpired', '$ramdom')";
+    my $ret = undef;
+    eval{
+        $ret = $pg->db->query($sql);
+    };
+    if ($@) {
+        $utils->logger($sql);
+        $utils->logger($@);
+        return undef;
+        #exit 1;
+    }
+    #$utils->logger($sql) if DEBUG==1;
+    my $next = $pg->db->query("select nextval('dtb_customer_customer_id_seq')");
+    my $nextv = $next->hash->{'nextval'};
+    return $nextv;
+}
+
 sub updateMember
 {
     my ($pg,$line) =@_;
@@ -221,5 +262,47 @@ sub updateMember
     return 1;
 
 }
+
+sub updateOnline
+{
+    my ($pg,$line) =@_;
+    my $utils = ImportCsv::Commons::Utils->new;
+    my $dt = Moment->now->get_dt();
+    my $sex = $line->[1];
+    for(my $i=0; $i< keys $line; $i++) {$line->[$i] =~ s/'/''/g;}
+    # $line->[19]保有ポイントTBD
+    $line->[19] = 0 if ( !$line->[19]);
+    # $line->[20]ポイント有効期限
+    my $pexpired = $line->[20];
+    if ( length($line->[20]) ){
+        my $y = substr($line->[20],0,4);
+        my $m = substr($line->[20],4,2);
+        my $d = substr($line->[20],6,2);
+        $pexpired = sprintf("%4s-%2s-%2s 00:00:00", $y,$m,$d);
+    }else{$pexpired = '1970-01-01 00:00:00';}
+    # $line->[21]支払い状況は会員の場合NULL
+    # $line->[22]会員状況(2)
+    $line->[22] = ($line->[22] =~ s/^0+//);
+    # secret_key
+    my $ramdom = $utils->generate_str();
+    my $sql = 'UPDATE dtb_customer ';
+    $sql .= "status=$line->[22],sex=$line->[1],pref=$line->[2],name01='$line->[3]',name02='$line->[4]',kana01='$line->[5]',";
+    $sql .= " kana02='$line->[6]',company_name='$line->[7]',company_name2='$line->[8]',zip01='$line->[9]',zip02='$line->[10]',";
+    $sql .= "addr01='$line->[11]',addr02='$line->[12]',addr03='$line->[13]',tel01='$line->[14]',tel02='$line->[15]',fax01='$line->[16]',";
+    $sql .= "note='$line->[24]',update_date='$dt',customer_type_id=$line->[0],realize_point=$line->[19],point_expiration_date='$pexpired'";
+    $sql .= " WHERE client_code='$line->[17]'";
+    my $ret = undef;
+    eval{
+        $ret = $pg->db->query($sql);
+    };
+    if ($@) {
+        $utils->logger($sql);
+        $utils->logger($@);
+        return undef;
+    }
+    return 1;
+
+}
+
 1;
 
