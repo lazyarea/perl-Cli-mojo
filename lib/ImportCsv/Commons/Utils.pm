df8bcb657f55667a43e0856fcd54216808d931df
My Name <kazuhisa@lazyarea.com>
2016-12-20 03:22:00 +0000

    add logger

--- lib/ImportCsv/Commons/Utils.pm
+++ lib/ImportCsv/Commons/Utils.pm
@@ -1,9 +1,12 @@
 package ImportCsv::Commons::Utils;
 
 use Mojo::Base qw/Mojolicious::Command/;
+use Mojo::Log;
 use Getopt::Long qw(GetOptionsFromArray :config no_auto_abbrev no_ignore_case);
 use constant DEBUG => 0;
 use constant DATA_DIR => '/var/www/doc/data';
+use constant LOG_DIR  => '/tmp';
+use constant LOG_FILE  => '/mojo.log';
 use Text::CSV;
 use Data::Dumper;
 
@@ -27,6 +30,13 @@ sub get_file_name
             return $file;
         }
     }
+}
+
+sub addLog
+{
+    my ($self,$data) = @_;
+    my $log = Mojo::Log->new(path => LOG_DIR.LOG_FILE, level => 'info');
+    $log->info($data);
 }
 
 1;
