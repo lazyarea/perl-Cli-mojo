4c31dc6a590d2e99c0e9fad7b5c26072af3d7261
My Name <kazuhisa@lazyarea.com>
2016-12-27 08:25:25 +0000

    会員種別

---
+++ lib/ImportCsv/Data/Dtb/CustomerKind.pm
@@ -0,0 +1,37 @@
+package ImportCsv::Data::Dtb::CustomerKind;
+
+use Mojo::Base qw/Mojolicious::Command/;
+use Getopt::Long qw(GetOptionsFromArray :config no_auto_abbrev no_ignore_case);
+use Mojo::Pg;
+use Text::CSV;
+use File::Copy;
+use ImportCsv::Data::Base;
+use Moment;
+use Data::Dumper;
+
+use constant DEBUG => 0; # 1:true
+
+has commons_config => sub {
+    my $config = ImportCsv::Commons::Config->new;
+    $config->load_config();
+};
+has utils => sub{
+     return ImportCsv::Commons::Utils->new;
+};
+
+sub find
+{
+    my ($self,$pg,$cond) = @_;
+    return undef if (!$cond);
+    my $sql = "select * from dtb_customer_kind";
+    $sql .= " WHERE 1=1 ";
+    for( keys $cond){$sql .= " AND $_ = '$cond->{$_}'";}
+    $sql .= " LIMIT 1";
+    my $query = $pg->db->query($sql);
+    my $hash  = $query->hash;
+    return $hash;
+}
+
+
+1;
+
