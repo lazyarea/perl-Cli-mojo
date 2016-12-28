6932c4c20d54067cdee24b8ce3ceb460f2c66348
My Name <kazuhisa@lazyarea.com>
2016-12-28 07:01:54 +0000

    add rank

---
+++ lib/ImportCsv/Data/Mtb/BlackRank.pm
@@ -0,0 +1,38 @@
+package ImportCsv::Data::Mtb::BlackRank;
+
+use Mojo::Base qw/Mojolicious::Command/;
+use Getopt::Long qw(GetOptionsFromArray :config no_auto_abbrev no_ignore_case);
+use Mojo::Pg;
+use ImportCsv::Data::Base;
+use Moment;
+use Data::Dumper;
+
+use constant DEBUG => 0; # 1:true
+
+has commons_config => sub {
+    my $config = ImportCsv::Commons::Config->new;
+    $config->load_config();
+};
+has utils => sub{
+     return ImportCsv::Commons::Utils->new;
+};
+
+sub get_black_rank_id
+{
+    my ($self, $pg, $val) = @_;
+    my $utils = ImportCsv::Commons::Utils->new;
+    my $sql = "SELECT id from mtb_black_rank WHERE name like '$val%'";
+    my $ret = undef;
+    local $@;
+    eval{
+        $ret = $pg->db->query($sql);
+    };
+    if ($@) {
+        $utils->logger($sql);
+        $utils->logger($@);
+    }
+    my $hash = $ret->hash;
+    return $hash;
+}
+
+1;
